b079ccd74d0e1d78449bd927b5e0ba5f4570cf4e
Boneman <129086760+o-boneman@users.noreply.github.com>
2023-04-01 16:51:12 -0300

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,62 +1,38 @@
-const typewriter = (elementId, texts, speed, blinkSpeed, pauseBetweenTexts, loopCount) => {
+// Typewriter function
+const typewriter = (elementId, texts, speed, pauseBetweenTexts, loopCount, hideOnEnd = false) => {
   let index = 0;
   let currentTextIndex = 0;
   let currentLoopCount = 0;
   const element = document.getElementById(elementId);
 
   const type = () => {
-    if (currentLoopCount < loopCount && currentTextIndex < texts.length) {
+    if ((loopCount === 0 || currentLoopCount < loopCount) && currentTextIndex < texts.length) {
       if (index < texts[currentTextIndex].length) {
         element.innerHTML = texts[currentTextIndex].slice(0, index) + '<span class="cursor">|</span>';
         index++;
         setTimeout(type, speed);
       } else {
-        blink(2, blinkSpeed);
+        setTimeout(() => {
+          index = 0;
+
+          if (!(loopCount > 0 && !hideOnEnd && texts.length === 1 && currentLoopCount + 1 === loopCount)) {
+            element.innerHTML = '';
+          }
+
+          currentTextIndex++;
+
+          if (currentTextIndex >= texts.length) {
+            currentTextIndex = 0;
+            currentLoopCount++;
+          }
+
+          if (loopCount === 0 || currentLoopCount < loopCount) {
+            type();
+          }
+        }, pauseBetweenTexts);
       }
-    }
-  };
-
-  const blink = (remainingBlinks, blinkSpeed) => {
-    if (remainingBlinks > 0) {
-      element.style.visibility = element.style.visibility === 'hidden' ? 'visible' : 'hidden';
-      remainingBlinks--;
-      setTimeout(() => blink(remainingBlinks, blinkSpeed), blinkSpeed);
-    } else {
-      element.style.visibility = 'visible';
-      setTimeout(() => {
-        index = 0;
-        element.innerHTML = '';
-        currentTextIndex++;
-
-        if (currentTextIndex >= texts.length) {
-          currentTextIndex = 0;
-          currentLoopCount++;
-        }
-
-        if (currentLoopCount < loopCount) {
-          type();
-        }
-      }, pauseBetweenTexts);
     }
   };
 
   type();
 };
-
-// Add CSS for the cursor
-const style = document.createElement('style');
-style.innerHTML = `
-  .cursor {
-    animation: blink 1s infinite;
-  }
-
-  @keyframes blink {
-    50% {
-      opacity: 0;
-    }
-  }
-`;
-document.head.appendChild(style);
-
-// Customize the texts, element ID, typing speed, blink speed, pause between texts, and loop count as needed
-typewriter("typewriter", ["This is the first text.", "And this is the second one!"], 100, 500, 1000, 3);
